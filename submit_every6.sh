3dbfbb4e192018e8c59895bae887ff07e09e19b4
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2024-07-11 09:15:46 +0200

    increase runtime

--- submit_every6.sh
+++ submit_every6.sh
@@ -2,8 +2,7 @@
 #SBATCH --job-name=lorenz96_every6
 #SBATCH --output=lorenz96_every6_%j.out
 #SBATCH --error=lorenz96_every6_%j.err
-#SBATCH --time 120
-#SBATCH --qos 2h
+#SBATCH --time 2-00:00:00
 #SBATCH -p gpu
 #SBATCH -G RTX5000
 #SBATCH --mem 32G
--- submit_every7.sh
+++ submit_every7.sh
@@ -2,8 +2,7 @@
 #SBATCH --job-name=lorenz96_every7
 #SBATCH --output=lorenz96_every7_%j.out
 #SBATCH --error=lorenz96_every7_%j.err
-#SBATCH --time 120
-#SBATCH --qos 2h
+#SBATCH --time 2-00:00:00
 #SBATCH -p gpu
 #SBATCH -G RTX5000
 #SBATCH --mem 32G
