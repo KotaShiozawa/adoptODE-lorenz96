fe1ab40feb01568d1a976684fc43e3ec3be2fd13
Inga Kottlarz <inga.kottlarz@ds.mpg.de>
2024-08-08 15:01:00 +0200

    increase max runtime and memory

--- submit_every7.sh
+++ submit_every7.sh
@@ -2,10 +2,11 @@
 #SBATCH --job-name=lorenz96_every7
 #SBATCH --output=lorenz96_every7_%j.out
 #SBATCH --error=lorenz96_every7_%j.err
-#SBATCH --time 2-00:00:00
+#SBATCH --time 7-00:00:00
+#SBATCH --qos 7d
 #SBATCH -p gpu
 #SBATCH -G RTX5000
-#SBATCH --mem 32G
+#SBATCH --mem 100G
 
 
 source .venv/bin/activate
